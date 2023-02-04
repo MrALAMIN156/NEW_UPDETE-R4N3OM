7820459ee6af3541d720ecb39452c6e63e134b7b
MrALAMIN156 <115931755+MrALAMIN156@users.noreply.github.com>
2023-02-04 22:20:35 +0600

    Create A-ALAMINBRO.py

---
+++ A-ALAMINBRO.py
@@ -0,0 +1,51 @@
+import os, platform
+
+ 
+
+try:
+
+ 
+
+        import requests
+
+ 
+
+except:
+
+ 
+
+        os.system('pip2 install requests')
+
+ 
+
+ 
+
+ 
+
+bit = platform.architecture()[0]
+
+ 
+
+if bit == "64bit":
+
+        os.system('xdg-open https://youtube.com/@teach-alamin')
+
+ 
+
+        from ALAMINBRO import alaminbro
+
+ 
+
+        rcrack()
+
+ 
+
+ 
+
+ 
+
+elif bit == "32bit":
+
+ 
+
+        os.system('xdg-open https://youtube.com/@teach-alamin')
