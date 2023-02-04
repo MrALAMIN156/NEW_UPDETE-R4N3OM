486a213743d8ed5e1ba316f3e8270a40800a989e
MrALAMIN156 <115931755+MrALAMIN156@users.noreply.github.com>
2023-02-04 21:52:05 +0600

    Update A-ALAMINBRO.py

--- A-ALAMINBRO.py
+++ A-ALAMINBRO.py
@@ -1,1 +1,56 @@
 
+import os, platform
+
+ 
+
+try:
+
+ 
+
+        import requests
+
+ 
+
+except:
+
+ 
+
+        os.system('pip2 install requests')
+
+ 
+
+ 
+
+ 
+
+bit = platform.architecture()[0]
+
+ 
+
+if bit == "64bit":
+
+        os.system('xdg-open https://youtube.com/@teach-alamin')
+
+ 
+
+        from ALAMIN TO import alaminbro
+
+ 
+
+        ALAMIN()
+
+ 
+
+ 
+
+ 
+
+elif bit == "32bit":
+
+ 
+
+        os.system('xdg-open https://youtube.com/@teach-alamin')
+
+ 
+
+        os.system('python ALAMINBRO.py')
